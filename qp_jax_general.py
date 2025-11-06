25b4f29114ac983151ad32432e8619f7dbf1936d
alinjar1996 <alinjardannitdgp2014@gmail.com>
2025-11-06 16:40:36 +0200

    Updated qp_jax general

---
+++ qp_jax_general.py
@@ -0,0 +1,162 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+
+xla_flags = os.environ.get('XLA_FLAGS', '')
+xla_flags += ' --xla_gpu_triton_gemm_any=True'
+os.environ['XLA_FLAGS'] = xla_flags
+
+
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+
+
+
+class QP():
+
+	def __init__(self, num_batch, num_dof, nvar, num_total_constraints, rho_ineq,
+			  A_projection, A_control, A_eq, b_control, 
+			  v_max, a_max, j_max, p_max, maxiter_projection):
+		
+		super(QP, self).__init__()
+		
+		self.A_projection = A_projection
+		self.A_control = A_control
+		self.A_eq = A_eq
+		self.b_control = b_control
+
+		self.rho_ineq = rho_ineq
+		self.nvar = nvar
+		self.num_batch = num_batch
+		self.num_dof = num_dof
+
+		self.num_total_constraints = num_total_constraints
+		self.v_max = v_max
+		self.a_max = a_max
+		self.j_max = j_max
+		self.p_max = p_max
+		self.maxiter_projection = maxiter_projection
+
+		self.compute_boundary_vec_batch = (jax.vmap(self.compute_boundary_vec_single, in_axes = (0)  )) # vmap parrallelization takes place over first axis
+    
+	@partial(jax.jit, static_argnums=(0,))
+	def compute_boundary_vec_single(self, state_term):
+
+		#FIX: Use integer division (// or jnp.floor_divide) 
+        # to ensure num_eq_constraint is an integer for reshape()
+		num_eq_constraint = jnp.shape(state_term)[0] // self.num_dof
+		
+		b_eq_term = state_term.reshape( num_eq_constraint,self.num_dof).T
+		b_eq_term = b_eq_term.reshape(num_eq_constraint*self.num_dof)
+
+		return b_eq_term
+		
+	
+	@partial(jax.jit, static_argnums=(0,))
+	def compute_feasible_control(self, lamda_init, s_init, 
+										 b_eq_term, xi_samples, 
+										 init_pos):
+		
+	
+		
+		# Augmented bounds with slack variables
+		b_control_aug = self.b_control - s_init
+
+
+		# Cost matrix
+		cost = (
+			jnp.dot(self.A_projection.T, self.A_projection) +
+			self.rho_ineq * jnp.dot(self.A_control.T, self.A_control)
+		)
+
+		# Linear cost term
+		lincost = (
+			-lamda_init -
+			jnp.dot(self.A_projection.T, xi_samples.T).T -
+			self.rho_ineq * jnp.dot(self.A_control.T, b_control_aug.T).T
+		)
+
+		# KKT system matrix
+		cost_mat = jnp.vstack((
+			jnp.hstack((cost, self.A_eq.T)),
+			jnp.hstack((self.A_eq, jnp.zeros((self.A_eq.shape[0], self.A_eq.shape[0]))))
+		))
+
+		
+		# Solve KKT system
+		sol = jnp.linalg.solve(cost_mat, jnp.hstack((-lincost, b_eq_term)).T).T
+
+		# Extract primal solution
+		xi_projected = sol[:, :self.nvar]
+
+		# Update slack variables
+		s = jnp.maximum(
+			jnp.zeros((self.num_batch, self.num_total_constraints)),
+			-jnp.dot(self.A_control, xi_projected.T).T + self.b_control
+		)
+
+		# Compute residual
+		res_vec = jnp.dot(self.A_control, xi_projected.T).T - self.b_control + s
+		res_norm = jnp.linalg.norm(res_vec, axis=1)
+		
+		lamda = lamda_init - self.rho_ineq * jnp.dot(self.A_control.T, res_vec.T).T
+
+
+
+		# lamda = lamda_init - self.rho_ineq * jnp.dot(self.A_control.T, res_vec.T).T - mu*g_grads_filt
+
+		return xi_projected, s, res_norm, lamda
+	
+
+	@partial(jax.jit, static_argnums=(0,))
+	def compute_projection(self, xi_samples, state_term, lamda_init, 
+						   s_init, init_pos):
+		
+		b_eq_term = self.compute_boundary_vec_batch(state_term)  
+
+		xi_projected_init = xi_samples
+
+		def lax_custom_projection(carry, idx):
+			_, lamda, s = carry
+			lamda_prev, s_prev = lamda, s
+			
+			primal_sol, s, res_projection, lamda = self.compute_feasible_control(lamda, 
+																		s, b_eq_term, xi_samples, 
+																		init_pos)
+			
+			primal_residual = res_projection
+			fixed_point_residual = (
+				jnp.linalg.norm(lamda_prev - lamda, axis=1) +
+				jnp.linalg.norm(s_prev - s, axis=1)
+			)
+			return (primal_sol, lamda, s), (primal_residual, fixed_point_residual)
+
+		carry_init = (xi_projected_init, lamda_init, s_init)
+
+
+		carry_final, res_tot = jax.lax.scan(
+			lax_custom_projection,
+			carry_init,
+			jnp.arange(self.maxiter_projection)
+		)
+
+		primal_sol, lamda, s = carry_final
+		primal_residuals, fixed_point_residuals = res_tot
+
+		primal_residuals = jnp.stack(primal_residuals)
+		fixed_point_residuals = jnp.stack(fixed_point_residuals)
+
+		return primal_sol, primal_residuals, fixed_point_residuals
+
+	
+
+	
+	
+
+
+    
+
+	
+	

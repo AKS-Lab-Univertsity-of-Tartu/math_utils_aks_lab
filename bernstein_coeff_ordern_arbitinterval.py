c34107f3999038b930c769a4c2b4eecffe77af0b
alinjar1996 <alinjardannitdgp2014@gmail.com>
2025-11-22 19:06:34 +0200

    Pint calculationa dded in Bernstein. Not verified though.

--- bernstein_coeff_ordern_arbitinterval.py
+++ bernstein_coeff_ordern_arbitinterval.py
@@ -28,4 +28,23 @@ def bernstein_coeff_ordern_new(n, tmin, tmax, t_actual):
         for i in range(n + 1)
     ]).squeeze().T / (l**2)
 
-    return P, Pdot, Pddot
+    #Integral of the Bernstein polynomial (Pint)
+    n_plus_1 = n + 1
+    P_n_plus_1 = np.array([
+        binom(n_plus_1, j) * (1 - t)**(n_plus_1 - j) * t**j
+        for j in range(n_plus_1 + 1)
+    ])
+    
+    # The normalized integral (Pint_norm) of B_{i,n}(t) from 0 to t is:
+    # integral_0^t B_{i,n}(\tau) d\tau = 1/(n+1) * Sum_{j=i+1}^{n+1} B_{j, n+1}(t)
+    Pint_norm = np.zeros(n + 1)
+    for i in range(n + 1):
+        # Sum B_{j, n+1}(t) for j from i+1 up to n+1.
+        # Python slicing P_n_plus_1[i + 1:] corresponds to j >= i + 1.
+        Pint_norm[i] = (1 / n_plus_1) * np.sum(P_n_plus_1[i + 1:])
+
+    # Denormalize: Pint = l * Pint_norm
+    # Since d(t_actual) = l * d(t)
+    Pint = l * Pint_norm
+
+    return P, Pdot, Pddot, Pint
